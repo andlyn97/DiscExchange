f1fb1fcea4d3cc95974c46213669dff90e1dba20
andlyn97 <57588500+andlyn97@users.noreply.github.com>
2021-10-12 17:16:50 +0200

    Initial commit

---
+++ app/src/main/java/gr7/discexchange/model/Bruker.java
@@ -0,0 +1,10 @@
+package gr7.discexchange.model;
+
+public class Bruker {
+
+    private String navn;
+    private String adresse;
+    private int rangering;
+    private double butikkKredit;
+    
+}
