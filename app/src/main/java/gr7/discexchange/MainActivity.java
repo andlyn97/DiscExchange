56b5a2b2b3291334411cbe3d28ea36f4ae96dc10
andlyn97 <57588500+andlyn97@users.noreply.github.com>
2021-10-13 10:54:18 +0200

    Rebase

--- app/src/main/java/gr7/discexchange/MainActivity.java
+++ app/src/main/java/gr7/discexchange/MainActivity.java
@@ -1,18 +1,38 @@
 package gr7.discexchange;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-
-
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        signOutBtn = findViewById(R.id.signOutBtn);
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance()
+                        .signOut(getApplicationContext())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getApplicationContext(), "User logged out", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
     }
 
 
