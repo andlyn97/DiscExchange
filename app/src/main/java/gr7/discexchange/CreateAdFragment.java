6f42fc2b7ef5a7665a69acf46aa3ed0b9dc24ee3
andlyn97 <57588500+andlyn97@users.noreply.github.com>
2021-10-20 21:56:27 +0200

    Starta på fragment for å "lage annonser", og opprettet fragment for "mine annonser"

---
+++ app/src/main/java/gr7/discexchange/CreateAdFragment.java
@@ -0,0 +1,19 @@
+package gr7.discexchange;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CreateAdFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_ad, container, false);
+    }
+}
