e8f84a0c292e81239a7100c5c3c329719f067c37
andlyn97 <57588500+andlyn97@users.noreply.github.com>
2021-10-21 19:54:18 +0200

    Feed fungerer 100%

--- app/src/main/java/gr7/discexchange/MyFeedFragment.java
+++ app/src/main/java/gr7/discexchange/MyFeedFragment.java
@@ -8,9 +8,20 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import gr7.discexchange.adapter.AdRecycleAdapter;
 import gr7.discexchange.model.Ad;
@@ -18,6 +29,12 @@ import gr7.discexchange.model.Ad;
 
 public class MyFeedFragment extends Fragment {
 
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    CollectionReference adCollectionReference = firebaseFirestore.collection("ad");
+    List<Ad> ads = new ArrayList<>();
+
+    RecyclerView adRecyclerView;
+    AdRecycleAdapter adAdapter;
 
     public MyFeedFragment() {
         // Required empty public constructor
@@ -37,10 +54,48 @@ public class MyFeedFragment extends Fragment {
     }
 
     private void setupRecyclerView(View view) {
-        RecyclerView adRecyclerView = view.findViewById(R.id.adRecyclerView);
+        adRecyclerView = view.findViewById(R.id.adRecyclerView);
+        adAdapter = new AdRecycleAdapter(view.getContext(), ads);
 
-        adRecyclerView.setAdapter(new AdRecycleAdapter(view.getContext(), Ad.getData()));
+        adRecyclerView.setAdapter(adAdapter);
 
         adRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
     }
+
+    private void createFirestoreReadListner() {
+        adCollectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for(QueryDocumentSnapshot adDocumentSnapshot : task.getResult()) {
+                        Ad ad = adDocumentSnapshot.toObject(Ad.class);
+                        ad.setUid(adDocumentSnapshot.getId());
+                        ads.add(ad);
+                    }
+                    adAdapter.notifyDataSetChanged();
+                } else {
+                    Log.d("Debug12", "Error:" + task.getException());
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        createFirestoreReadListner();
+    }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+
+
 }
