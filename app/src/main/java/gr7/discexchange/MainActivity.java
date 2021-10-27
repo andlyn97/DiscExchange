ccb0bd9ed626a1762784e61acf4673c48b38ea28
andlyn97 <57588500+andlyn97@users.noreply.github.com>
2021-10-27 21:14:33 +0200

    Prøvde litt med databinding, navigasjonen forsvinner når jeg tukler med xml

--- app/src/main/java/gr7/discexchange/MainActivity.java
+++ app/src/main/java/gr7/discexchange/MainActivity.java
@@ -1,38 +1,23 @@
 package gr7.discexchange;
 
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.makeramen.roundedimageview.RoundedImageView;
-
-import gr7.discexchange.model.User;
 import gr7.discexchange.viewmodel.DEViewModel;
 
 public class MainActivity extends AppCompatActivity{
@@ -41,9 +26,6 @@ public class MainActivity extends AppCompatActivity{
     private TextView navAddressTextView;
     private RoundedImageView navImageProfilePic;
     private DEViewModel viewModel;
-    private ListenerRegistration userFirestoreListenerRegistration;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +33,10 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         viewModel = new ViewModelProvider(this).get(DEViewModel.class);
-
         drawerLayout = findViewById(R.id.drawer_layout);
 
 
-        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+        findViewById(R.id.imageMenu).setOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
 
         NavigationView drawerNavView = findViewById(R.id.navigationView);
         BottomNavigationView bottomNavView = findViewById(R.id.bottomNav);
@@ -84,16 +60,13 @@ public class MainActivity extends AppCompatActivity{
         navController.setGraph(navGraph);
 
 
-        viewModel.getUser().observe(this, new Observer<User>() {
-            @Override
-            public void onChanged(User user) {
-                if(user == null) {
-                    return;
-                }
-                navUserNameTextView.setText(user.getName());
-                navAddressTextView.setText(user.getAddress());
-                Glide.with(getApplicationContext()).load(user.getImageUrl()).into(navImageProfilePic);
+        viewModel.getUser().observe(this, user -> {
+            if(user == null) {
+                return;
             }
+            navUserNameTextView.setText(user.getName());
+            navAddressTextView.setText(user.getAddress());
+            Glide.with(getApplicationContext()).load(user.getImageUrl()).into(navImageProfilePic);
         });
 
 
@@ -113,8 +86,6 @@ public class MainActivity extends AppCompatActivity{
         final TextView textTitle = findViewById(R.id.textTitle);
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> textTitle.setText(destination.getLabel()));
-
-        setupFirestoreListener();
     }
 
     private void logout(NavigationView drawerNavView) {
@@ -127,39 +98,5 @@ public class MainActivity extends AppCompatActivity{
                     });
             return true;
         });
-    }
-
-    public void setupFirestoreListener() {
-        /*userFirestoreListenerRegistration = FirebaseFirestore.getInstance().collection("user").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (error != null) {
-                    return;
-                }
-
-                for (DocumentChange docChange : value.getDocumentChanges()) {
-                    User userFromFS = docChange.getDocument().toObject(User.class);
-                    userFromFS.setUid(docChange.getDocument().getId());
-
-                    if (docChange.getDocument().getId().equals(FirebaseAuth.getInstance().getUid())) {
-                        viewModel.setUser(userFromFS);
-                    }
-                }
-            }
-        });*/
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //setupFirestoreListener();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(userFirestoreListenerRegistration != null) {
-            userFirestoreListenerRegistration.remove();
-        }
     }
 }
--- app/src/main/java/gr7/discexchange/ProfileFragment.java
+++ app/src/main/java/gr7/discexchange/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
@@ -19,6 +20,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.makeramen.roundedimageview.RoundedImageView;
+
 
 import gr7.discexchange.model.User;
 import gr7.discexchange.viewmodel.DEViewModel;
